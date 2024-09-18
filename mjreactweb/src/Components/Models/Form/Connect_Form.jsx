3ba695eaf2ca8a6dc074be9e02986ccee6d210c4
Deepshikhafrontend <deep.shikha.frontend@gmail.com>
2024-09-18 19:25:59 +0530

    changes

--- mjreactweb/src/Components/Models/Form/Connect_Form.jsx
+++ mjreactweb/src/Components/Models/Form/Connect_Form.jsx
@@ -6,8 +6,10 @@ import Custom_Centered_DynamicDialog from "@/Components/Models/Dialog/Center_Dia
 import { contactFormApi } from "@/Utils/api"
 import { useEffect, useState } from "react";
 import "./styles.scss"
-import { Calendar } from 'primereact/calendar';
 
+import { Dropdown } from 'primereact/dropdown';
+import { Calendar } from 'primereact/calendar';
+import { MultiSelect } from 'primereact/multiselect';
 import { Button as PrimeButtton } from 'primereact/button';
 
 const {
@@ -23,7 +25,8 @@ const Connect_Form = ({ modelState = false, setModelState }) => {
     const [datetime12h, setDateTime12h] = useState(null);
     const [visible, setVisible] = useState(false);
     const { register, handleSubmit, formState: { errors } } = useForm()
-
+    const [selectedServices, setSelectedServices] = useState("");
+    const [countryState, setCountryState] = useState("");
     const [loading, setLoading] = useState(false);
 
     const submitForm = async (data) => {
@@ -38,6 +41,9 @@ const Connect_Form = ({ modelState = false, setModelState }) => {
                 data.time = time
                 data.date = date
             }
+            data.country = countryState
+            data.service = data?.service ? data?.service.join(", ") : ""
+
             const requestOptions = {
                 method: "POST",
                 headers: {
@@ -61,6 +67,23 @@ const Connect_Form = ({ modelState = false, setModelState }) => {
         }
         setLoading(false)
     };
+    const serviceList = ["Customer Insight", "Design", "Consulting", "Details", "Digital", "Content Services", "Marketing Services"]
+
+    const selectedCountryTemplate = (option, props) => {
+        if (option) {
+            return (
+                <div>{`(+${option.phone}) ${option.name}`} </div>
+            );
+        }
+
+        return <span>{props.placeholder}</span>;
+    };
+
+    const countryOptionTemplate = (option) => {
+        return (
+            <div>{`(+${option.phone}) ${option.name}`} </div>
+        );
+    };
 
     return (
         <Custom_Centered_DynamicDialog modelWidth={"100%"} modelHeight={"calc(100vh - var(--header-h) - 30px)"} state={modelState} setModelState={setModelState}>
@@ -79,9 +102,15 @@ const Connect_Form = ({ modelState = false, setModelState }) => {
                                 {errors?.email && <p>{errors?.email.message}</p>}
                             </div>
                             <div className="field">
-                                <select {...register("country")} defaultValue={"India"} placeholder="Country Code" >
+                                {/* <select {...register("country")} defaultValue={"India"} placeholder="Country Code" >
                                     {countries.map((country, i) => <option value={country.name} key={i}>{country.name} {` (+${country.phone})`}</option>)}
-                                </select>
+                                </select> */}
+                                <Dropdown filter filterBy="name" value={countryState}
+                                    defaultValue={"India"}
+                                    onChange={(e) => setCountryState(e.value)} options={countries} optionLabel={"name"}
+                                    optionValue="name"
+                                    valueTemplate={selectedCountryTemplate} itemTemplate={countryOptionTemplate}
+                                    placeholder="Select Country Code" className="w-full pr " />
                                 {errors?.code && <p>{errors?.code.message}</p>}
                             </div>
                             <div className="field">
@@ -92,16 +121,9 @@ const Connect_Form = ({ modelState = false, setModelState }) => {
                                 <input {...register("company")} type="text" placeholder="company" />
                             </div>
                             <div className="field">
-                                <select  {...register("service")} name="service" id="service">
-                                    <option value="">Choose Service</option>
-                                    <option value="Customer Insight">Customer Insight</option>
-                                    <option value="Design">Design</option>
-                                    <option value="Consulting">Consulting</option>
-                                    <option value="Details">Details</option>
-                                    <option value="Digital">Digital</option>
-                                    <option value="Content Services">Content Services</option>
-                                    <option value="Marketing Services">Marketing Services</option>
-                                </select>
+                                <MultiSelect {...register("service")} options={serviceList}
+                                    value={selectedServices} onChange={(e) => setSelectedServices(e.value)}
+                                    placeholder="Choose Service" className="pr w-full" />
                             </div>
                             <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }} className="field time">
                                 {visible && <div className={`time-model ${visible ? "active" : ""} `}>
