f28f70344ec773db87663f369e0628c60d5d50b8
Deepshikhafrontend <deep.shikha.frontend@gmail.com>
2024-09-18 19:32:01 +0530

    changes

--- mjreactweb/src/Components/CoverPage.jsx
+++
@@ -1,6 +0,0 @@
-
-function CoverPageImage({ src, loading = "lazy" }) {
-    return <img loading={loading} style={{ objectFit: "cover" }} height={250} width={"100%"} src={src} alt={src} />
-}
-
-export default CoverPageImage
--- mjreactweb/src/Utils/api.js
+++
@@ -1,4 +0,0 @@
-export const baseUrl = "https://mjreactweb.mediajenie.com/api";
-export const contactFormApi =
-  "https://mjreactweb.mediajenie.com/api/addContactDetails";
-export const resourceLink = "https://mediajenie.com/category/blog/";
